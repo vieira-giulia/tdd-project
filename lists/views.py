c12afc650ba794dd5433b844644e5e1455d50fdc
Giulia Vieira <giuliavieira@yahoo.com.br>
2020-10-19 10:05:19 -0300

    Entrada inválida não deve ser salva no BD

--- lists/views.py
+++ lists/views.py
@@ -10,21 +10,18 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': my_list})
 
 def new_list(request):
-    my_list = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect(f'/lists/{my_list.id}/')
+    list_ = List.objects.create()
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+    return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
-
-def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-    except ValidationError:
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
