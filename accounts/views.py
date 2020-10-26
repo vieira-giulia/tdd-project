3953dd10e6b63e9d00007fc70c067f272fb66e11
Giulia Vieira <giuliavieira@yahoo.com.br>
2020-10-26 09:34:19 -0300

    Checking That We Send the User a Link with a Token

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,19 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.contrib import messages
+from accounts.models import Token
+from django.core.urlresolvers import reverse
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'Use this link to log in',
+        message_body,
         'noreply@superlists',
         [email]
     )
