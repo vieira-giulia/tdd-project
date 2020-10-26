abb0686b6607ae4f0b05b3d89285c4af20ba92ad
Giulia Vieira <giuliavieira@yahoo.com.br>
2020-10-26 09:50:14 -0300

    Patching at the Class Level

--- accounts/views.py
+++ accounts/views.py
@@ -27,5 +27,6 @@ def send_login_email(request):
 
 def login(request):
     user = auth.authenticate(uid=request.GET.get('token'))
-    auth.login(request, user)
+    if user:
+        auth.login(request, user)
     return redirect('/')
